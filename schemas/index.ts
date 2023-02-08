9906e3c5785f717a90305c6f119d43de70897583
aenesyvz <20060403@stu.omu.edu.tr>
2023-02-08 23:06:07 +0300

    backend

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+import pin from './pin';
+import comment from './comment';
+import user from './user';
+import postedBy from './postedBy';
+import save from './save';
+export const schemaTypes = [
+    comment,
+    user,
+    postedBy,
+    save,
+    pin
+]
